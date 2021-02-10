a53ba77c7a8f243314251f5039b6a2df418c5703
celestialevermore <key2317@naver.com>
2021-02-10 19:56:52 +0900

    20210209

---
+++ Baekjoon/Baekjoon/로봇청소기24991.cpp
@@ -0,0 +1,235 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<queue>
+#define MAX 21
+using namespace std;
+
+char map[MAX][MAX];
+
+bool visited[MAX][MAX];
+
+int w = -1, h = -1;
+
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int totalmovecnt = 0;
+typedef struct Robot {
+	int x;
+	int y;
+	int havetoclean = 0;
+	int movecnt = 0;
+	
+}rrobot;
+
+
+queue<rrobot> Q;
+
+
+
+int cnt = 0;
+int ans[10000];
+
+
+void BFS(rrobot start) {
+	rrobot current;
+
+
+	while (!Q.empty()) {
+
+		current.x = Q.front().x;
+		current.y = Q.front().y;
+		current.havetoclean = Q.front().havetoclean;
+		current.movecnt = Q.front().movecnt;
+
+		Q.pop();
+
+
+		printf("(%d,%d) 현재 좌표에서 탐색을 시작합니다. \n\n", current.x, current.y);
+
+
+
+		if (current.havetoclean == 0) {
+			printf("청소가 완료되었습니다!!\n\n");
+			printf("청소 완료 시점에서의 이동 최소 횟수 : %d\n\n",totalmovecnt);
+			ans[cnt] = totalmovecnt;
+			cnt++;
+			break;
+		}
+
+
+
+
+		for (int i = 0; i < 4; i++) {
+
+			int x = current.x + dx[i];
+			int y = current.y + dy[i];
+
+			if (x < 0 || y < 0 || x >= h || y >= w) {
+				printf("(%d,%d) : 해당 좌표는 지도의 범위를 벗어납니다. \n", x, y);
+				printf("현재 이동 횟수는 %d 입니다. \n\n", current.movecnt);
+				continue;
+			}
+			if (visited[x][y]) {
+				printf("(%d,%d) : 해당 좌표는 이미 방문을 하였습니다. \n", x, y);
+				printf("현재 이동 횟수는 %d 입니다. \n\n", current.movecnt);
+				continue;
+			}
+			if (map[x][y] == 'x') {
+				printf("(%d,%d) : 해당 좌표는 가구입니다. \n", x, y);
+				printf("현재 이동 횟수는 %d 입니다. \n\n", current.movecnt);
+			}
+			if (!visited[x][y] && map[x][y] == '.') {
+				printf("(%d,%d) : 해당 좌표는 깨끗합니다. \n", x, y);
+				printf("현재 이동 횟수는 %d 입니다. \n\n", current.movecnt);
+				visited[x][y] = true;
+				rrobot next;
+				next.x = x;
+				next.y = y;
+				next.movecnt = current.movecnt;
+				next.havetoclean = current.havetoclean;
+				next.movecnt++;
+				Q.push(next);
+
+			}
+			if (!visited[x][y] && map[x][y] == '*') {
+				printf("\n\n<<<(%d,%d) : 해당 좌표는 더럽습니다. 청소를 진행해야 합니다. >>>\n\n\n\n", x, y);
+				visited[x][y] = true;
+				map[x][y] = '.';
+				rrobot newstart;
+				newstart.x = x;
+				newstart.y = y;
+				newstart.movecnt = current.movecnt;
+				newstart.havetoclean = current.havetoclean;
+				
+				newstart.movecnt++;
+				newstart.havetoclean--;
+				totalmovecnt = newstart.movecnt+totalmovecnt;
+				
+				printf("현재 이동 횟수는 %d 입니다. \n\n", totalmovecnt);
+				newstart.movecnt = 0;
+				
+				//printf("여태까지의 최소 이동횟수는 %d 입니다. \n\n",totalmovecnt);
+				printf("<<여기에서 좌표(%d,%d)를 새로운 시작점으로 삼습니다.>> \n\n\n",newstart.x,newstart.y);
+				printf("이때 총 이동횟수 : %d / 현재 이동횟수는 %d로 초기화합니다. \n\n", totalmovecnt, newstart.movecnt);
+				printf("\n\n<<큐를 초기화합니다.>>\n\n");
+				while (!Q.empty()) {
+					Q.pop();
+				}
+
+
+				Q.push(newstart);
+				
+			}
+
+
+
+
+
+		}
+	}
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+				
+			printf("%c ", map[i][j]);
+		
+		}
+		printf("\n");
+	}
+
+
+
+
+	int flag = 0;
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			if (map[i][j] == '*') {
+				printf("(%d,%d) : 아직 청소하지 않은 지점이 있습니다. \n\n", i, j);
+				flag = 1;
+				break;
+			}
+		}
+	}
+
+	if (flag == 1) {
+		ans[cnt] = -1;
+		cnt++;
+
+	}
+
+
+
+
+
+}
+
+
+int main() {
+
+	while (1) {
+
+		cin >> w >> h;
+		if (w == 0 && h == 0) { break; }
+		
+		
+		else{
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				cin >> map[i][j];
+			}
+		}
+		//시작점을 정합니다. 
+		rrobot start;
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				
+				if (map[i][j] == '*') {
+					start.havetoclean++;
+					printf("현재까지 확인된 청소해야 할 지점의 개수는 %d 입니다.\n\n", start.havetoclean);
+				}
+
+				if (map[i][j] == 'o') {
+					start.x = i;
+					start.y = j;
+					start.movecnt = 0;
+					visited[start.x][start.y] = true;
+			
+				}
+			}
+		}
+		printf("<<<(%d,%d): 이 지점에 로봇청소기가 존재합니다. 청소를 시작합니다.>>>\n\n\n", start.x, start.y);
+		printf("청소해야 할 지점의 개수는 %d 입니다 \n\n", start.havetoclean);
+		Q.push(start);
+		BFS(start);
+
+
+
+		//초기화
+		totalmovecnt = 0;
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				visited[i][j] = false;
+				map[i][j] = ' ';
+			}
+		}
+
+
+
+
+
+
+	}
+
+		
+
+	}
+
+
+
+	for (int i = 0; i < cnt; i++) {
+		printf("%d\n", ans[i]);
+	}
+}
+
+
+
--- Baekjoon/Baekjoon/빙산.cpp
+++ Baekjoon/Baekjoon/빙산.cpp
@@ -1,205 +1,205 @@
-#include<stdio.h>
-#include<iostream>
-#include<vector>
-#define MAX 300
-using namespace std;
-
-int N, M;
-
-int glacier[MAX][MAX];
-bool visited[MAX][MAX];
-int melting[MAX][MAX];
-
-
-int yearcnt = 0;
-int glaciercntlist[MAX];
-int glaciercnt = 0;
-int glacierheight[11];
-
-int dx[4] = { -1,1,0,0 };
-int dy[4] = { 0,0,1,-1 };
-
-
-void Meltinginfo() {
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (glacier[i][j] != 0) {
-				int meltinginfo = 0;
-				int x = i, y = j;
-				for (int i = 0; i < 4; i++) {
-					int xx = x + dx[i];
-					int yy = y + dy[i];
-					if (xx < 0 || y < 0 || xx >= N || yy >= M) { continue; }
-					if (glacier[xx][yy] == 0) {
-						meltinginfo++;
-					}
-				}
-				melting[x][y] = meltinginfo;
-			}
-		}
-	}
-
-	printf("\n\n<<다음은 현재 시점에서 녹는범위에 대한 정보입니다. >>\n\n");
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			printf("%d ", melting[i][j]);
-		}
-		printf("\n");
-	}
-
-
-}
-
-
-void Gomelting() {
-	Meltinginfo();
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			glacier[i][j] = glacier[i][j] - melting[i][j];
-			if (glacier[i][j] < 0) { glacier[i][j] = 0; }
-		}
-		
-	}
-}
-bool iszero() {
-	int flag = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (glacier[i][j] == 0) {
-				continue;
-			}
-			else {
-				flag = 1; break;
-			}
-		}
-	}
-	if (flag == 0) { return true; }
-	else { return false; }
-}
-
-
-
-void DFS(pair<int,int>start) {
-	
-	for (int i = 0; i < 4; i++) {
-		int x = start.first + dx[i];
-		int y = start.second + dy[i];
-
-		if (x < 0 || y < 0 || x >= N || y >= M) {
-			printf("좌표(%d,%d) : 범위를 넘어갑니다. \n\n",x,y);
-			continue;
-		}
-		if (visited[x][y]) { continue; }
-		if (glacier[x][y] != 0 && !visited[x][y]) {
-			visited[x][y] = true;
-			printf("좌표(%d,%d) %d : 아직 녹지 않은 빙산에 해당하므로 탐색을 진행합니다. \n\n", x, y,glacier[x][y]);
-			pair<int, int>next;
-			next.first = x;
-			next.second = y;
-			DFS(next);
-			
-		}
-
-
-
-	}
-	return;
-
-}
-int main() {
-
-	cin >> N >> M;
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> glacier[i][j];
-		}
-	}
-
-	//안녹고 뭔가 남아있는 동안.
-	while (!iszero()) {
-		//일단 녹인다.
-		Gomelting();
-		yearcnt++;
-		printf("<<%d년 현재 용해를 진행한 빙하의 정보는 다음과 같습니다.>> \n\n", yearcnt);
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				printf("%d ", glacier[i][j]);
-			}
-			printf("\n");
-		}
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (glacier[i][j] != 0 && !visited[i][j]) {
-					visited[i][j] = true;
-					printf("\n좌표 (%d,%d) : 이 좌표는 아직 물에 녹지 않았으므로 여기에서 DFS탐색을 시작합니다. \n\n", i, j);
-					pair<int, int>start;
-					start.first = i;
-					start.second = j;
-					DFS(start);
-					glaciercntlist[glaciercnt]++;
-						
-					printf("\n\n%d년이 지났을 때 확인되는 빙하의 개수는 %d 입니다.\n\n", yearcnt, glaciercntlist[glaciercnt]);
-					
-
-				}
-			}
-		}
-		
-		glaciercnt++;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				visited[i][j] = false;
-			}
-		}
-
-	}
-
-	for (int i = 0; i < glaciercnt; i++) {
-		printf("%d ", glaciercntlist[i]);
-	}
-	printf("\n");
-
-
-
-
-
-
-
-
-
-	int ans = 0;
-	int flag_ = 0;
-	for (int i = 0; i < glaciercnt; i++) {
-		//아무리 녹여도 빙산이 하나밖에 없는 경우
-		if (glaciercntlist[i] == 1) {
-			printf("%d년이 지난 시점에서는 아직 빙하가 1개입니다. \n\n", i+1);
-			continue;
-		}
-		if (glaciercntlist[i] > 1) {
-			flag_ = 1;
-			ans = i + 1;
-			break;
-		}
-		else { continue; }
-	}
-	
-
-	if (flag_ == 0) {
-		printf("0");
-	}
-	else {
-		printf("%d", ans);
-	}
-
-
-	
-
-
-
-
-
-}
+//#include<stdio.h>
+//#include<iostream>
+//#include<vector>
+//#define MAX 300
+//using namespace std;
+//
+//int N, M;
+//
+//int glacier[MAX][MAX];
+//bool visited[MAX][MAX];
+//int melting[MAX][MAX];
+//
+//
+//int yearcnt = 0;
+//int glaciercntlist[MAX];
+//int glaciercnt = 0;
+//int glacierheight[11];
+//
+//int dx[4] = { -1,1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//
+//
+//void Meltinginfo() {
+//
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < M; j++) {
+//			if (glacier[i][j] != 0) {
+//				int meltinginfo = 0;
+//				int x = i, y = j;
+//				for (int i = 0; i < 4; i++) {
+//					int xx = x + dx[i];
+//					int yy = y + dy[i];
+//					if (xx < 0 || y < 0 || xx >= N || yy >= M) { continue; }
+//					if (glacier[xx][yy] == 0) {
+//						meltinginfo++;
+//					}
+//				}
+//				melting[x][y] = meltinginfo;
+//			}
+//		}
+//	}
+//
+//	printf("\n\n<<다음은 현재 시점에서 녹는범위에 대한 정보입니다. >>\n\n");
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < M; j++) {
+//			printf("%d ", melting[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//
+//}
+//
+//
+//void Gomelting() {
+//	Meltinginfo();
+//
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < M; j++) {
+//			glacier[i][j] = glacier[i][j] - melting[i][j];
+//			if (glacier[i][j] < 0) { glacier[i][j] = 0; }
+//		}
+//		
+//	}
+//}
+//bool iszero() {
+//	int flag = 0;
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < M; j++) {
+//			if (glacier[i][j] == 0) {
+//				continue;
+//			}
+//			else {
+//				flag = 1; break;
+//			}
+//		}
+//	}
+//	if (flag == 0) { return true; }
+//	else { return false; }
+//}
+//
+//
+//
+//void DFS(pair<int,int>start) {
+//	
+//	for (int i = 0; i < 4; i++) {
+//		int x = start.first + dx[i];
+//		int y = start.second + dy[i];
+//
+//		if (x < 0 || y < 0 || x >= N || y >= M) {
+//			printf("좌표(%d,%d) : 범위를 넘어갑니다. \n\n",x,y);
+//			continue;
+//		}
+//		if (visited[x][y]) { continue; }
+//		if (glacier[x][y] != 0 && !visited[x][y]) {
+//			visited[x][y] = true;
+//			printf("좌표(%d,%d) %d : 아직 녹지 않은 빙산에 해당하므로 탐색을 진행합니다. \n\n", x, y,glacier[x][y]);
+//			pair<int, int>next;
+//			next.first = x;
+//			next.second = y;
+//			DFS(next);
+//			
+//		}
+//
+//
+//
+//	}
+//	return;
+//
+//}
+//int main() {
+//
+//	cin >> N >> M;
+//
+//	for (int i = 0; i < N; i++) {
+//		for (int j = 0; j < M; j++) {
+//			cin >> glacier[i][j];
+//		}
+//	}
+//
+//	//안녹고 뭔가 남아있는 동안.
+//	while (!iszero()) {
+//		//일단 녹인다.
+//		Gomelting();
+//		yearcnt++;
+//		printf("<<%d년 현재 용해를 진행한 빙하의 정보는 다음과 같습니다.>> \n\n", yearcnt);
+//		for (int i = 0; i < N; i++) {
+//			for (int j = 0; j < M; j++) {
+//				printf("%d ", glacier[i][j]);
+//			}
+//			printf("\n");
+//		}
+//
+//		for (int i = 0; i < N; i++) {
+//			for (int j = 0; j < M; j++) {
+//				if (glacier[i][j] != 0 && !visited[i][j]) {
+//					visited[i][j] = true;
+//					printf("\n좌표 (%d,%d) : 이 좌표는 아직 물에 녹지 않았으므로 여기에서 DFS탐색을 시작합니다. \n\n", i, j);
+//					pair<int, int>start;
+//					start.first = i;
+//					start.second = j;
+//					DFS(start);
+//					glaciercntlist[glaciercnt]++;
+//						
+//					printf("\n\n%d년이 지났을 때 확인되는 빙하의 개수는 %d 입니다.\n\n", yearcnt, glaciercntlist[glaciercnt]);
+//					
+//
+//				}
+//			}
+//		}
+//		
+//		glaciercnt++;
+//		for (int i = 0; i < N; i++) {
+//			for (int j = 0; j < M; j++) {
+//				visited[i][j] = false;
+//			}
+//		}
+//
+//	}
+//
+//	for (int i = 0; i < glaciercnt; i++) {
+//		printf("%d ", glaciercntlist[i]);
+//	}
+//	printf("\n");
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//	int ans = 0;
+//	int flag_ = 0;
+//	for (int i = 0; i < glaciercnt; i++) {
+//		//아무리 녹여도 빙산이 하나밖에 없는 경우
+//		if (glaciercntlist[i] == 1) {
+//			printf("%d년이 지난 시점에서는 아직 빙하가 1개입니다. \n\n", i+1);
+//			continue;
+//		}
+//		if (glaciercntlist[i] > 1) {
+//			flag_ = 1;
+//			ans = i + 1;
+//			break;
+//		}
+//		else { continue; }
+//	}
+//	
+//
+//	if (flag_ == 0) {
+//		printf("0");
+//	}
+//	else {
+//		printf("%d", ans);
+//	}
+//
+//
+//	
+//
+//
+//
+//
+//
+//}
