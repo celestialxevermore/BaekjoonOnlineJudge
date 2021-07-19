61c7412f238daf320f608e1955a72135a631dba7
celestial_evermore <key2317@naver.com>
2021-07-19 18:06:00 +0900

    Revert "20210601"
    This reverts commit 810c12d5fe72681414b2a595a224420a71476dd4.

--- Baekjoon/Baekjoon/유니온파이드.cpp
+++ Baekjoon/Baekjoon/유니온파이드.cpp
@@ -1,51 +1,51 @@
-//#include<iostream>
-//#include<vector>
-//
-//using namespace std;
-//
-//
-//int N, M;
-//int friends[1001];
-//
-//int Find(int a) {
-//	if (a == friends[a]) return a;
-//	else {
-//		return Find(friends[a]);
-//	}
-//}
-//
-//
-//
-//void Union(int snode, int tnode) {
-//
-//	int sn = Find(snode);
-//	int tn = Find(tnode);
-//
-//	if (sn != tn) {
-//		friends[snode] = tnode;
-//	}
-//
-//
-//}
-//
-//
-//
-//int main() {
-//
-//
-//	cin >> N >> M;
-//
-//	for (int i = 1; i <= N; i++) {
-//		friends[i] = i;
-//	}
-//
-//
-//	for (int i = 0; i < M; i++) {
-//		int snode, tnode;
-//		cin >> snode >> tnode;
-//
-//		Union(snode, tnode);
-//	}
-//
-//
-//}
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+int N, M;
+int friends[1001];
+
+int Find(int a) {
+	if (a == friends[a]) return a;
+	else {
+		return Find(friends[a]);
+	}
+}
+
+
+
+void Union(int snode, int tnode) {
+
+	int sn = Find(snode);
+	int tn = Find(tnode);
+
+	if (sn != tn) {
+		friends[snode] = tnode;
+	}
+
+
+}
+
+
+
+int main() {
+
+
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++) {
+		friends[i] = i;
+	}
+
+
+	for (int i = 0; i < M; i++) {
+		int snode, tnode;
+		cin >> snode >> tnode;
+
+		Union(snode, tnode);
+	}
+
+
+}
