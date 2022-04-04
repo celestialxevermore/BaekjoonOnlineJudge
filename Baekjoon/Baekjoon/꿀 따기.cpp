65f7eef03ca307b3a274a41fe4c60e86b1f3d715
celestialevermore <key2317@naver.com>
2022-04-04 22:31:31 +0900

    나도 좆밥인걸로~

---
+++ Baekjoon/Baekjoon/꿀 따기.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<vector>
+//
+//#include<algorithm>
+//#define MAX 100001
+//using namespace std;
+//
+//int bee1=0, bee2=0, home = 0;
+//
+//int place[MAX];
+//
+//
+//int main() {
+//
+//
+//
+//
+//
+//}
--- Baekjoon/Baekjoon/수열.cpp
+++ Baekjoon/Baekjoon/수열.cpp
@@ -1,47 +1,47 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#define MAX 100001
-using namespace std;
-
-int map[1000][MAX];
-int input[MAX];
-int N, K;
-
-int main() {
-
-	cin >> N >> K;
-
-	for (int i = 1; i <= N; i++) {
-		cin >> input[i];
-		map[1][i] = input[i];
-
-
-	}
-	
-
-
-
-	for (int window = 2; window <= K; window++) {
-
-		for (int cur = window; cur <= N; cur++) {
-
-			map[window][cur] = map[window - 1][cur] + input[cur-window+1];
-
-
-		}
-
-
-
-	}
-
-
-	int maxi = -100000;
-	for (int i = 1; i <= MAX; i++) {
-		maxi = max(maxi, map[K][i]);
-	}
-
-	cout << maxi;
-
-
-}
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//#define MAX 100001
+//using namespace std;
+//
+//int map[1000][MAX];
+//int input[MAX];
+//int N, K;
+//
+//int main() {
+//
+//	cin >> N >> K;
+//
+//	for (int i = 1; i <= N; i++) {
+//		cin >> input[i];
+//		map[1][i] = input[i];
+//
+//
+//	}
+//	
+//
+//
+//
+//	for (int window = 2; window <= K; window++) {
+//
+//		for (int cur = window; cur <= N; cur++) {
+//
+//			map[window][cur] = map[window - 1][cur] + input[cur-window+1];
+//
+//
+//		}
+//
+//
+//
+//	}
+//
+//
+//	int maxi = -100000;
+//	for (int i = 1; i <= MAX; i++) {
+//		maxi = max(maxi, map[K][i]);
+//	}
+//
+//	cout << maxi;
+//
+//
+//}
